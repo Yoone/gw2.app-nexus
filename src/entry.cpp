258851a3e943d1b0c8a7a58dd28571cd08e12a88
Yoann Bentz <yoann@yoone.eu>
2026-08-24 23:06:57 -0400

    Bump to v0.2.0.0

--- src/entry.cpp
+++ src/entry.cpp
@@ -51,7 +51,7 @@ extern "C" __declspec(dllexport) AddonDefinition_t* GetAddonDef()
 
     AddonDef.Name = "GW2.app";
     AddonDef.Version.Major = 0;
-    AddonDef.Version.Minor = 1;
+    AddonDef.Version.Minor = 2;
     AddonDef.Version.Build = 0;
     AddonDef.Version.Revision = 0;
     AddonDef.Author = "Yoone";
